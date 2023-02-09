2c05ccc0a368bd1308084774578f33f7bea55b95
zhangwb <804158121@qq.com>
2023-02-09 15:55:57 +0800

    > remove redundant lines

--- lib/tools/tree_view/src/models/node_parent.dart
+++ lib/tools/tree_view/src/models/node_parent.dart
@@ -20,8 +20,6 @@ import 'dart:convert';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_demo_previewer/tools/tree_view/widget.dart';
 
-import 'widget.dart';
-
 /// Defines the data used to display a [TreeNode].
 ///
 /// Used by [TreeView] to display a [TreeNode].
