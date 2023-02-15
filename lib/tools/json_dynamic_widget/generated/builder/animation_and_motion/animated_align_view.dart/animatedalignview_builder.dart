53f69547a2f59f5e89fa9bb8fbcd8b84e6a34326
zhangwb <804158121@qq.com>
2023-02-15 10:26:22 +0800

    Track builder and register in pre branch

---
+++ lib/tools/json_dynamic_widget/generated/builder/animation_and_motion/animated_align_view.dart/animatedalignview_builder.dart
@@ -0,0 +1,30 @@
+import 'file:C:/Users/12700/Documents/FlutterProjects/Src/widget_design/lib/src/views/animation_and_motion/animated_align_view.dart';
+import 'package:flutter/material.dart';
+import 'package:json_dynamic_widget/json_dynamic_widget.dart';
+import 'package:child_builder/child_builder.dart';
+/// replace  '[Json2widgetTemp]' with [className]
+/// replace  [animated_align_view.dart] with [fileName]
+class AnimatedAlignViewBuilder extends JsonWidgetBuilder {
+  const AnimatedAlignViewBuilder({
+    super.numSupportedChildren = 0,
+  });
+
+  static const type = 'animated_align_view.dart';
+
+  static AnimatedAlignViewBuilder fromDynamic(
+    dynamic map, {
+    JsonWidgetRegistry? registry,
+  }) {
+    return const AnimatedAlignViewBuilder();
+  }
+    @override
+  Widget buildCustom({
+    ChildWidgetBuilder? childBuilder,
+    required BuildContext context,
+    required JsonWidgetData data,
+    Key? key,
+  }) {
+    
+    return const AnimatedAlignView();
+  }
+}
