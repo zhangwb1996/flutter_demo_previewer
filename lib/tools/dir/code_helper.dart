0027921f60551dedadfbc3ab07285a5f229fd284
zhangwb <804158121@qq.com>
2023-02-19 14:46:07 +0800

    improvement

--- lib/tools/dir/code_helper.dart
+++ lib/tools/dir/code_helper.dart
@@ -5,7 +5,7 @@
 /// Created Date: Sunday, 2023-02-19 11:12:54 am
 /// Author: Wenbo Zhang (zhangwb1996@outlook.com)
 /// -----
-/// Last Modified: Sunday, 2023-02-19 12:56:16 pm
+/// Last Modified: Sunday, 2023-02-19 2:38:57 pm
 /// Modified By: Wenbo Zhang (zhangwb1996@outlook.com)
 /// -----
 /// Copyright (c) 2023
@@ -22,6 +22,9 @@ import 'dart:io';
 /// pass [key] as file path
 /// return the code in this file path
 String codeHelper(String key) {
+  if (!key.endsWith(".dart")) {
+    return '';
+  }
   List<String> code = File(key).readAsLinesSync();
   code.removeWhere((e) => e.startsWith('///'));
 
